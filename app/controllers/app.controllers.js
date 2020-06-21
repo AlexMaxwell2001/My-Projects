c705716d54d0abd51c5138b3dbc005883679e746
Alex Maxwell <66850447+AlexMaxwell2001@users.noreply.github.com>
2020-06-21 23:55:41 +0100

    Update app.controllers.js

--- app/controllers/app.controllers.js
+++ app/controllers/app.controllers.js
@@ -1,4 +1,3 @@
-//When the app is in the root location retrieve all results from the database, these are used to render the view
 exports.root = (req, res) => {
     res.render('Number_view');
     
